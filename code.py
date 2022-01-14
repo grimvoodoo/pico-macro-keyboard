953a9fc9a977938547126175068ac75999707f43
acleveland <andrew.cleveland@hotmail.co.uk>
2022-01-14 01:01:11 +0000

    AC - added Rotation dials and LED functionality

--- code.py
+++ code.py
@@ -1,87 +1,149 @@
+import conf
 import time
 import digitalio
 import board
-import conf
-
-exclusions = set(list(["_", "GP0", "GP28_A2"]))
-board_list = set(list(dir(board)))
-pin_list = ["GP15", "GP14"]
-
-# filters out the bad entries from the GPIO pins
-# for x in board_list:
-#     for y in exclusions:
-#         if y in x:
-#             break
-#         else:
-#             pin_list.append(x)
+import rotaryio
+import usb_hid
+import adafruit_character_lcd.character_lcd as character_lcd
+from adafruit_hid.consumer_control import ConsumerControl
+from adafruit_hid.consumer_control_code import ConsumerControlCode
 
 
-def button(key):
-    btn_pin = getattr(board, key)
-    btn = digitalio.DigitalInOut(btn_pin)
-    btn.direction = digitalio.Direction.INPUT
-    btn.pull = digitalio.Pull.DOWN
-    return btn
+# USB device
+# consumer = ConsumerControl(usb_hid.devices)
+
+# button delay
+# dl = 0.2
+
+# loop
+# while True:
+#
+#     # poll encoder button
+#     if conf.rotate_button.value == 0:
+#         consumer.send(ConsumerControlCode.MUTE)
+#         conf.led.value = True
+#         time.sleep(dl)
+#         conf.led.value = False
+#     time.sleep(0.1)
+
+# def button(key):
+#     btn_pin = getattr(board, key)
+#     btn = digitalio.DigitalInOut(btn_pin)
+#     btn.direction = digitalio.Direction.INPUT
+#     btn.pull = digitalio.Pull.DOWN
+#     return btn
+#
+#
+
 
 
 def key_detection():
+    mode = 1
     # for x in pin_list:
     #     btn = button(x)
     while True:
+        if conf.previous_value != conf.rotate_step.value:
+            if not conf.rotate_step.value:
+                if not conf.rotate_direction.value:
+                    print("Rotated Right")
+                    if mode < 3:
+                        mode += 1
+                    else:
+                        mode = 1
+                    time.sleep(0.5)
+                else:
+                    print("Rotated left")
+                    if mode > 1:
+                        mode -= 1
+                    else:
+                        mode = 3
+                    time.sleep(0.5)
+                    time.sleep(0.5)
+            conf.previous_value = conf.rotate_step.value
+        if not conf.rotate_button.value:
+            print(f"MODE {mode}: rotary button pressed")
+            time.sleep(0.5)
         if conf.btn15.value:
-            print(f"button GP15 pressed")
-            time.sleep(0.2)
+            print(f"MODE {mode}: button GP15 pressed")
+            conf.led.value = True
+            time.sleep(1)
+            conf.led.value = False
+            # if mode == 1:
+            # elif mode == 2:
+            # elif mode == 3:
+            time.sleep(0.5)
         if conf.btn14.value:
-            print(f"button GP14 pressed")
-            time.sleep(0.2)
-
-
-# btn1_pin = board.GP15
-# btn1 = digitalio.DigitalInOut(btn1_pin)
-# btn1.direction = digitalio.Direction.INPUT
-# btn1.pull = digitalio.Pull.DOWN
-
-
-# def button_pressed():
-#     print(board.board_id)
-#     print(digitalio.DigitalInOut.value)
-    # print(button["gpio"])
-    # while True:
-    #     if pin:
-    #         print(f"{btn} pressed")
-    #     time.sleep(0.1)
-
-
-# button_pressed()
-# for gp in button:
-#     pin = getattr(board, gp)
-#     btn = digitalio.DigitalInOut(pin)
-#     btn.direction = digitalio.Direction.INPUT
-#     btn.pull = digitalio.Pull.DOWN
-#     while True:
-#         if btn.value:
-#             print(f"{gp} pressed")
-#             time.sleep(0.1)
-
-
-# btn1_pin = board.GP15
-# btn1 = digitalio.DigitalInOut(btn1_pin)
-# btn1.direction = digitalio.Direction.INPUT
-# btn1.pull = digitalio.Pull.DOWN
-#
-# btn2_pin = board.GP14
-# btn2 = digitalio.DigitalInOut(btn2_pin)
-# btn2.direction = digitalio.Direction.INPUT
-# btn2.pull = digitalio.Pull.DOWN
-
-# while True:
-#     if btn1.value:
-#         print("button 1 pressed")
-#     time.sleep(0.1)
-#
-#     if btn2.value:
-#         print("button 2 pressed")
-#     time.sleep(0.1)
+            print(f"MODE {mode}: button GP14 pressed")
+            # if mode == 1:
+            # elif mode == 2:
+            # elif mode == 3:
+            time.sleep(0.5)
+        if conf.btn13.value:
+            print(f"MODE {mode}: button GP13 pressed")
+            # if mode == 1:
+            # elif mode == 2:
+            # elif mode == 3:
+            time.sleep(0.5)
+        if conf.btn12.value:
+            print(f"MODE {mode}: button GP12 pressed")
+            # if mode == 1:
+            # elif mode == 2:
+            # elif mode == 3:
+            time.sleep(0.5)
+        if conf.btn11.value:
+            print(f"MODE {mode}: button GP11 pressed")
+            # if mode == 1:
+            # elif mode == 2:
+            # elif mode == 3:
+            time.sleep(0.5)
+        if conf.btn10.value:
+            print(f"MODE {mode}: button GP10 pressed")
+            # if mode == 1:
+            # elif mode == 2:
+            # elif mode == 3:
+            time.sleep(0.5)
+        if conf.btn9.value:
+            print(f"MODE {mode}: button GP9 pressed")
+            # if mode == 1:
+            # elif mode == 2:
+            # elif mode == 3:
+            time.sleep(0.5)
+        if conf.btn8.value:
+            print(f"MODE {mode}: button GP8 pressed")
+            # if mode == 1:
+            # elif mode == 2:
+            # elif mode == 3:
+            time.sleep(0.5)
+        if conf.btn7.value:
+            print(f"MODE {mode}: button GP7 pressed")
+            # if mode == 1:
+            # elif mode == 2:
+            # elif mode == 3:
+            time.sleep(0.5)
+        if conf.btn6.value:
+            print(f"MODE {mode}: button GP6 pressed")
+            # if mode == 1:
+            # elif mode == 2:
+            # elif mode == 3:
+            time.sleep(0.5)
+        # if conf.btn5.value:
+        #     print(f"button GP5 pressed")
+        #     time.sleep(0.2)
+        # if conf.btn4.value:
+        #     print(f"button GP4 pressed")
+        #     time.sleep(0.2)
+        # if conf.btn3.value:
+        #     print(f"button GP3 pressed")
+        #     time.sleep(0.2)
+        # if conf.btn2.value:
+        #     print(f"button GP2 pressed")
+        #     time.sleep(0.2)
+        # if conf.btn1.value:
+        #     print(f"button GP1 pressed")
+        #     time.sleep(0.2)
+        # if conf.btn0.value:
+        #     print(f"button GP0 pressed")
+        #     time.sleep(0.2)
 
 
 key_detection()
--- conf.py
+++ conf.py
@@ -1,5 +1,21 @@
 import board
 import digitalio
+import rotaryio
+import usb_hid
+from adafruit_hid.consumer_control import ConsumerControl
+from adafruit_hid.consumer_control_code import ConsumerControlCode
+import board
+import busio
+
+previous_value = True
+
+
+def rotary_encoder(key):
+    btn_pin = getattr(board, key)
+    btn = digitalio.DigitalInOut(btn_pin)
+    btn.direction = digitalio.Direction.INPUT
+    btn.pull = digitalio.Pull.UP
+    return btn
 
 
 def button(key):
@@ -10,11 +26,20 @@ def button(key):
     return btn
 
 
-btn1 = button("GP1")
-btn2 = button("GP2")
-btn3 = button("GP3")
-btn4 = button("GP4")
-btn5 = button("GP5")
+rotate_button = digitalio.DigitalInOut(board.GP18)
+rotate_button.direction = digitalio.Direction.INPUT
+rotate_button.pull = digitalio.Pull.UP
+lastPosition = 0
+
+
+led = digitalio.DigitalInOut(board.GP20)
+led.direction = digitalio.Direction.OUTPUT
+# btn0 = button("GP0")
+# btn1 = button("GP1")
+# btn2 = button("GP2")
+# btn3 = button("GP3")
+# btn4 = button("GP4")
+# btn5 = button("GP5")
 btn6 = button("GP6")
 btn7 = button("GP7")
 btn8 = button("GP8")
@@ -25,3 +50,9 @@ btn12 = button("GP12")
 btn13 = button("GP13")
 btn14 = button("GP14")
 btn15 = button("GP15")
+rotate_direction = rotary_encoder("GP16")
+rotate_step = rotary_encoder("GP17")
+# btn18 = button("GP18")
+# btn13 = rotary_encoder("GP13")
+# dirPin = rotary_encoder("GP14")
+# stepPin = rotary_encoder("GP15")
